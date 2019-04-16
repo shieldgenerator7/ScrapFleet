744f86600cbc3bba0ba46a6bdfa5f6d02fdb929a
shieldgenerator7 <shieldgenerator7@yahoo.com>
2019-04-15 23:52:48 -0400

    Deck Generator generates multiple cards

--- Assets/Scripts/DeckGenerator.cs
+++ Assets/Scripts/DeckGenerator.cs
@@ -6,6 +6,8 @@ using UnityEditor;
 
 public class DeckGenerator : MonoBehaviour
 {
+    public bool forceOneOfEach = false;//true to force only one of each card to be generated
+
     /// <summary>
     /// The string data of what to write in the cards
     /// [number of cards]:[pilot?][accuracy][fire rate][speed][shields][hull]
@@ -27,19 +29,25 @@ public class DeckGenerator : MonoBehaviour
 
     public void generate()
     {
-        string[] data = cardData[0].Split(':');
-        int count = int.Parse(data[0]);
-
-        CardGen.setStats(data[1]);
-        CardGen.generate();
-        Texture2D tex2d = CardGen.generateCardImage();
-        string baseFileName = "" + data[2].ToString().Trim() + "_";
-        for (int i = 1; i <= count; i++)
+        for (int i = 0; i < cardData.Count; i++)
         {
-            string filename = baseFileName + i + ".png";
-            SaveTextureToFile(tex2d, filename);
+            string[] data = cardData[i].Split(':');
+            int count = int.Parse(data[0]);
+            if (forceOneOfEach)
+            {
+                count = 1;
+            }
+            CardGen.setStats(data[1]);
+            CardGen.generate();
+            Texture2D tex2d = CardGen.generateCardImage();
+            string baseFileName = "" + data[2].ToString().Trim() + "_";
+            for (int j = 1; j <= count; j++)
+            {
+                string filename = baseFileName + j + ".png";
+                SaveTextureToFile(tex2d, filename);
+            }
         }
-        EditorUtility.RevealInFinder(Application.persistentDataPath+"/"+Application.productName);
+        EditorUtility.RevealInFinder(Application.persistentDataPath + "/" + Application.productName);
     }
 
     /// <summary>
