9bf9ce1f985e51b3f5561f88532350ae2a01d90e
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-01-06 22:00:39 -0500

    Added CardDataEditor

---
+++ Assets/Editor/CardDataEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CardData))]
+public class CardDataEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        CardData cardData = (CardData)target;
+
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Generate Card"))
+        {
+            CardGenerator cg = FindObjectOfType<CardGenerator>();
+            cg.cardData = cardData;
+            cg.generate();
+            Selection.activeGameObject = cg.gameObject;
+        }
+    }
+}
